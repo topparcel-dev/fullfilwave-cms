a7dbe443222dc31a604786a0d800b2a31999cf4e
Oleg Shadrin <redhexpt@gmail.com>
2025-11-20 16:56:29 +0000

    Add session management configuration to admin settings

--- config/admin.ts
+++ config/admin.ts
@@ -1,6 +1,13 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
+    sessions: {
+      accessTokenLifespan: env.int('ADMIN_SESSIONS_ACCESS_TOKEN_LIFESPAN', 1800), // 30 minutes
+      maxRefreshTokenLifespan: env.int('ADMIN_SESSIONS_MAX_REFRESH_TOKEN_LIFESPAN', 2592000), // 30 days
+      idleRefreshTokenLifespan: env.int('ADMIN_SESSIONS_IDLE_REFRESH_TOKEN_LIFESPAN', 604800), // 7 days
+      maxSessionLifespan: env.int('ADMIN_SESSIONS_MAX_SESSION_LIFESPAN', 604800), // 7 days
+      idleSessionLifespan: env.int('ADMIN_SESSIONS_IDLE_SESSION_LIFESPAN', 3600), // 1 hour
+    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
